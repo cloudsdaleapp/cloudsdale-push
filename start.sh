8a733ca2e9009d5ad1f0d27cfc5c63e60a0f2241
Philip Vieira <philip@vallin.se>
2012-06-10 17:05:22 +0200

    Make sure to checkout master.

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-(cd ./vendor/submodules/cloudsdale-faye && git pull --rebase && bundle && npm install && sleep 10 && node ./server.js)
+(cd ./vendor/submodules/cloudsdale-faye && git checkout master && git pull --rebase && bundle && npm install && sleep 10 && node ./server.js)
