a23f0979a4f7b61c8463e160ad5a8f76cd9627c0
Philip Vieira <philip@vallin.se>
2012-11-16 21:40:00 +0100

    Updated deploy script, IT NOW ACTUALLY WORKS!

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,21 +40,21 @@ default_run_options[:pty] = true
 namespace :deploy do
   task :default do
     update
-    start
+    restart
   end
 
   task :cold do
     update
     start
   end
-  
+
   task :setup, :expect => { :no_release => true } do
     dirs  = [deploy_to, releases_path, shared_path]
     dirs += shared_children.map { |d| File.join(shared_path, d) }
     run "mkdir -p #{dirs.join(' ')}"
     run "chmod g+w #{dirs.join(' ')}" if fetch(:group_writable, true)
   end
-  
+
   task :finalize_update, :except => { :no_release => true } do
     run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
     run <<-CMD
@@ -63,46 +63,32 @@ namespace :deploy do
       ln -s #{shared_path}/node_modules #{latest_release}/node_modules
     CMD
   end
-  
+
   task :start, :roles => :app do
-    # run "sudo -p restart #{application} || sudo -p start #{application}"
+    run "#{sudo} /etc/init.d/faye start"
   end
 
   task :stop, :roles => :app do
-    # run "#{sudo} stop #{application}"
+    run "#{sudo} /etc/init.d/faye stop"
   end
 
   task :restart, :roles => :app do
-    start
+    stop
   end
-  
+
   task :npm, :roles => :app do
     run <<-CMD
       export PATH=#{node_path}:$PATH &&
       cd #{latest_release} &&
-      npm install
+      rm #{latest_release}/node_modules &&
+      npm install &&
+      cd #{latest_release}/node_modules/faye &&
+      bundle install &&
+      jake &&
+      cd #{latest_release}
     CMD
   end
-  
-  task :write_upstart_script, :roles => :app do
-    upstart_script = <<-UPSTART_SCRIPT
-    
-description "#{application} upstart script"
-start on (local-filesystem and net-device-up)
-stop on shutdown
-respawn
-respawn limit 5 60
-script
-  chdir #{current_path}
-  exec sudo -u #{user} NODE_ENV="production" node #{node_script} >> log/production.log 2>&1
-end script
 
-    UPSTART_SCRIPT
-        
-    put upstart_script, "/tmp/#{application}.conf"
-    run "#{sudo} mv /tmp/#{application}.conf /etc/init"
-  end
 end
 
-after 'deploy:setup', 'deploy:write_upstart_script'
 after 'deploy:finalize_update', 'deploy:npm'
